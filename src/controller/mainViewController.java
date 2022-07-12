73766db7a85ca8cd643c1c18928e35ed0eadf78c
jayedumindu <jayedumindu@gmail.com>
2022-07-12 10:30:06 +0530

    jar added : view created

---
+++ src/controller/mainViewController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class mainViewController {
+    public TableView studentTbl;
+    public TableColumn sIdClm;
+    public TableColumn sNameClm;
+    public TableColumn sMailClm;
+    public TableColumn sContactClm;
+    public TableColumn sAddressClm;
+    public JFXTextField sNameTxt;
+    public JFXTextField sMailTxt;
+    public JFXTextField sContactTxt;
+    public JFXTextField sAddressTxt;
+    public JFXButton saveBtn;
+    public JFXTextField sidTxt;
+
+    public void saveOrUpdateStudent(ActionEvent actionEvent) {
+    }
+
+    public void removeStudent(ActionEvent actionEvent) {
+    }
+}
