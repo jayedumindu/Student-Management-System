9d3680281c0a5c32e61a7ff4a9f673055fda077f
jayedumindu <jayedumindu@gmail.com>
2022-07-12 11:04:03 +0530

    INSERT initialized created .java files

--- src/controller/mainViewController.java
+++ src/controller/mainViewController.java
@@ -2,9 +2,14 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import db.CrudUtil;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import model.Student;
+
+import java.sql.SQLException;
 
 public class mainViewController {
 
@@ -22,10 +27,20 @@ public class mainViewController {
     public JFXButton saveBtn;
     public JFXTextField sidTxt;
 
-    public void saveOrUpdateStudent(ActionEvent actionEvent) {
-
+    public void saveOrUpdateStudent(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Student st = new Student(sidTxt.getText(),sNameTxt.getText(),sContactTxt.getText(),sAddressTxt.getText(),sMailTxt.getText());
+        String statement = "INSERT INTO TABLE Student(?,?,?,?,?)";
+        if(CrudUtil.execute(statement,st.getSId(),st.getSName(),st.getContact(),st.getAddress(),st.getEMail())){
+            new Alert(Alert.AlertType.INFORMATION,"Student Saved!").show();
+        }else {
+            new Alert(Alert.AlertType.ERROR,"Interrupted!").show();
+        }
     }
 
     public void removeStudent(ActionEvent actionEvent) {
+
+    }
+
+    public void searchStudent(ActionEvent actionEvent) {
     }
 }
